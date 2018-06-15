52450624071bb897e9d46a2123235802b064b054
jake <tolstoi_l_n@naver.com>
2018-06-15 15:44:35 +0900

    Modified HTML

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.shortcuts import render
 from django.utils import timezone
 
@@ -11,4 +11,4 @@ from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {})
+    return render(request, 'blog/post_list.html', {'posts': posts})
